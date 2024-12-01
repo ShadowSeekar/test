0f3d7786175cfceeea080172af3b702aa003629d
Harshit <raoharshit2025@gmail.com>
2024-12-02 05:16:51 +0530

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -18,10 +18,17 @@ with open('tokenizer.pkl', 'rb') as file:
     tokenizer = pickle.load(file)
 
 # Function to preprocess text
-def preprocess_text(text, tokenizer, max_len=100):
-    sequences = tokenizer.texts_to_sequences([text])
-    padded = pad_sequences(sequences, maxlen=max_len)
-    return padded
+def preprocess_text(sen):
+    '''Cleans text data up, leaving only 2 or more char long non-stepwords composed of A-Z & a-z only in lowercase'''
+    sentence = sen.lower()
+    sentence = remove_tags(sentence)
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence) 
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+    sentence = re.sub(r'\s+', ' ', sentence) 
+    pattern = re.compile(r'\b(' + r'|'.join(stopwords.words('english')) + r')\b\s*')
+    sentence = pattern.sub('', sentence)
+
+    return sentence
 
 # Streamlit app
 st.title("Sentiment Analysis with LSTM")
@@ -31,14 +38,21 @@ st.write("Enter text to analyze its sentiment:")
 user_input = st.text_area("Text Input", placeholder="Type your text here...")
 
 if st.button("Analyze Sentiment"):
-    if user_input.strip():
+    unseen_processed = preprocess_text(user_input)
+    unseen_tokenized = tokenizer.texts_to_sequences(unseen_processed)
+    unseen_padded = pad_sequences(unseen_tokenized, padding='post', maxlen=100)
+    unseen_sentiments = model.predict(unseen_padded)
+
+
+
+#    if user_input.strip():
         # Preprocess input text
-        processed_text = preprocess_text(user_input, tokenizer)
+#        processed_text = preprocess_text(user_input, tokenizer)
         # Get the prediction (use model like a regular layer)
-        prediction = model(processed_text).numpy()[0][0]
-        sentiment = "Positive" if prediction > 0.5 else "Negative"
-        
-        st.write(f"**Sentiment Score:** {prediction:.2f}")
-        st.write(f"**Sentiment:** {sentiment}")
-    else:
-        st.write("Please enter valid text.")
+#        prediction = model(processed_text).numpy()[0][0]
+    sentiment = "Positive" if prediction > 0.5 else "Negative"
+    prediction = unseen_sentiments*10
+    st.write(f"**Sentiment Score:** {prediction:.2f}")
+    st.write(f"**Sentiment:** {sentiment}")
+else:
+    st.write("Please enter valid text.")
