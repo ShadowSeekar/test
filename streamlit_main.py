77fa31e38f8ef889dd97adc57f595cde50ce3bcd
Harshit <raoharshit2025@gmail.com>
2024-12-02 04:25:25 +0530

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
-# Load the trained LSTM model (SavedModel format)
-model_directory = "tf_model"  # Replace with your actual directory
+# Load the trained LSTM model from the 'tf_model' folder
+model_directory = "tf_model"  # Path to your model folder
 model = tf.keras.models.load_model(model_directory)
 
-# Load the tokenizer (if it was saved separately)
+# Load the tokenizer (ensure tokenizer.pkl is saved alongside the model)
 with open('tokenizer.pkl', 'rb') as file:
     tokenizer = pickle.load(file)
 
